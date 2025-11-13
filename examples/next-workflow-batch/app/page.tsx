b2bce17f59db49689da9ed1340b4af884b76c031
Adrian Cooney <cooney.adrian@gmail.com>
2025-11-13 21:47:52 +0000

    Add list batches

---
+++ examples/next-workflow-batch/app/page.tsx
@@ -0,0 +1,25 @@
+import { google } from "@ai-sdk/google";
+import { start } from "workflow/api";
+import { generate } from "./generate/generate";
+
+export default async function Page() {
+  const batches = (await google.languageModel("gemini-2.5-flash-lite").doListBatches?.()) || []
+
+  async function generateAction() {
+    "use server";
+
+    await start(generate);
+  }
+
+  return (
+    <div>
+      <h1>Batches: {batches.length}</h1>
+      {batches.map(batch => {
+        return <div><h4>{batch.id}</h4></div>
+      })}
+      <form action={generateAction}>
+        <button type="submit">Create Batch</button>
+      </form>
+    </div>
+  );
+}
